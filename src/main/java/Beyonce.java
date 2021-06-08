f1571fba21ef50951999e0384855c1e28672142d
tharanath.m <tharanath@thoughtworks.com>
2021-06-08 21:04:37 +0530

    My Halo is here sweetie

--- src/main/java/Beyonce.java
+++ src/main/java/Beyonce.java
@@ -1,2 +1,24 @@
-package PACKAGE_NAME;public class Beyonce {
+public class Beyonce {
+    private final int age = 42;
+    private final int experience = 20;
+
+    public void Lemonade() {
+        System.out.println("Jay Z's car has been smashed");
+    }
+
+    public void SpiceReunion() {
+        System.out.println("The Spice Girls have reunited again!");
+    }
+
+    public void YoungFame() {
+        int x = age - experience;
+        System.out.println("I was " + x + " years old when fame found me");
+    }
+
+    public static void main(String[] args) {
+        Beyonce bee = new Beyonce();
+        bee.Lemonade();
+        bee.YoungFame();
+        bee.SpiceReunion();
+    }
 }
